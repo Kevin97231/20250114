5d382c45c08caaa7dcfdda88fb02858ecf81701f
klabarre <klabarre@dawan.fr>
2025-01-14 10:23:38 +0100

    ajout cdn tailwind et daysiUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 /* eslint-disable react/prop-types */
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import JsxTheorie from "./components/JsxTheorie";
 
 function App() {
-  return <JsxTheorie />;
+  return (
+    <>
+      <BrowserRouter>
+        <p>NavBar</p>
+
+        <Routes>
+          <Route path="/" element={<JsxTheorie />}></Route>
+          <Route path="/test" element={<>Ma page test</>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
